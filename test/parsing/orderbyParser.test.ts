3523f82aeb9ad17caefbf75ccbda170b47498916
Steffen Huels <steffen.huels@sap.com>
2022-07-30 23:10:47 +0200

    orderBy Tests

---
+++ test/parsing/orderbyParser.test.ts
@@ -0,0 +1,189 @@
+import assert from "assert"
+import orderbyParser from "../../src/parsing/orderbyParser"
+import { NodeTypes, OrderbyNode } from "../../src/types/nodes"
+
+function testParsingAndAST(testcase: { type: string, input: string, expectedAST: any }) {
+    it(`should parse ${testcase.type}: ${testcase.input}`, () => {
+        let ast = orderbyParser.parse(testcase.input)
+        assert.deepStrictEqual(ast, testcase.expectedAST)
+    })
+}
+
+//ADD test from https://github.com/oasis-tcs/odata-abnf/blob/main/abnf/odata-abnf-testcases.yaml line 2467 when all parsers are finished
+describe('Orderby Parser tests', () => {
+    describe('official url-convention examples', () => {
+        [{
+            type: "single default asc",
+            input: "Country/Name",
+            expectedAST: <OrderbyNode>{
+                nodeType: NodeTypes.OrderbyNode,
+                value: [
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "asc",
+                        value: "Country/Name"
+                    }
+                ]
+            }
+        },
+        {
+            type: "multi default asc",
+            input: "Country/Name,City,Street",
+            expectedAST: <OrderbyNode>{
+                nodeType: NodeTypes.OrderbyNode,
+                value: [
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "asc",
+                        value: "Country/Name"
+                    },
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        "type": "asc",
+                        "value": "City"
+                    },
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        "type": "asc",
+                        "value": "Street"
+                    }
+                ]
+            }
+        },
+        {
+            type: "single specified asc",
+            input: "Country/Name asc",
+            expectedAST: <OrderbyNode>{
+                nodeType: NodeTypes.OrderbyNode,
+                value: [
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "asc",
+                        value: "Country/Name"
+                    }
+                ]
+            }
+        },
+        {
+            type: "multi specified asc",
+            input: "Country/Name asc,City asc,Street asc",
+            expectedAST: <OrderbyNode>{
+                nodeType: NodeTypes.OrderbyNode,
+                value: [
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "asc",
+                        value: "Country/Name"
+                    },
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        "type": "asc",
+                        "value": "City"
+                    },
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        "type": "asc",
+                        "value": "Street"
+                    }
+                ]
+            }
+        },
+        {
+            type: "single specified desc",
+            input: "Country/Name desc",
+            expectedAST: <OrderbyNode>{
+                nodeType: NodeTypes.OrderbyNode,
+                value: [
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "desc",
+                        value: "Country/Name"
+                    }
+                ]
+            }
+        },
+        {
+            type: "multi specified desc",
+            input: "Country/Name desc,City desc,Street desc",
+            expectedAST: <OrderbyNode>{
+                nodeType: NodeTypes.OrderbyNode,
+                value: [
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "desc",
+                        value: "Country/Name"
+                    },
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        "type": "desc",
+                        "value": "City"
+                    },
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        "type": "desc",
+                        "value": "Street"
+                    }
+                ]
+            }
+        },
+        {
+            type: "default asc and specified desc",
+            input: "Country/Name,Street desc",
+            expectedAST: <OrderbyNode>{
+                nodeType: NodeTypes.OrderbyNode,
+                value: [
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "asc",
+                        value: "Country/Name"
+                    },
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "desc",
+                        value: "Street"
+                    }
+                ]
+            }
+        },
+        {
+            type: "specified asc and desc",
+            input: "Country/Name asc,Street desc",
+            expectedAST: <OrderbyNode>{
+                nodeType: NodeTypes.OrderbyNode,
+                value: [
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "asc",
+                        value: "Country/Name"
+                    },
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "desc",
+                        value: "Street"
+                    }
+                ]
+            }
+        },
+        {
+            type: "default asc and specified asc",
+            input: "Country/Name,Street asc",
+            expectedAST: <OrderbyNode>{
+                nodeType: NodeTypes.OrderbyNode,
+                value: [
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "asc",
+                        value: "Country/Name"
+                    },
+                    {
+                        nodeType: NodeTypes.OrderbyItemNode,
+                        type: "asc",
+                        value: "Street"
+                    }
+                ]
+            }
+        }
+        ].forEach(testParsingAndAST)
+    });
+})
+
