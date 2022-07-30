cf7023b3e4f7d8a6d04d94d1d92fbba37978d58d
Steffen Huels <steffen.huels@sap.com>
2022-07-30 22:16:11 +0200

    skip and top parser tests

---
+++ test/parsing/topParser.test.ts
@@ -0,0 +1,45 @@
+import assert from "assert"
+import topParser from "../../src/parsing/topParser"
+
+describe('Skip Parser tests', () => {
+    describe('integer tests', () => {
+        [{
+            type: 'positive Integer',
+            input: '5',
+            expectedOutput: 5
+        },
+        {
+            type: 'Zero',
+            input: '0',
+            expectedOutput: 0
+        }
+        ].forEach(testcase => {
+            it(`should generate a valid ${testcase.type} Escape Identifier`, () => {
+                let parsedOutput = topParser.parse(testcase.input)
+                assert(parsedOutput == testcase.expectedOutput)
+            });
+        });
+    });
+    describe('error tests', () => {
+        [{
+            type: 'no Integer',
+            input: 'test'
+        },
+        {
+            type: 'negative Integer',
+            input: '-5'
+        }].forEach(testcase => {
+            it(`should throw Error because of ${testcase.type} Escape`, () => {
+                assert.throws(
+                    () => {
+                    topParser.parse(testcase.input);
+                    },
+                    {
+                    name: 'Error',
+                    message: 'top must be a valid integer value'
+                });
+            });
+        })
+    });
+
+});
