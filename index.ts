6be5ded9d24504996ccbe86c08cda44bfedbd048
lumaghg <rotkehlchen567@gmail.com>
2022-09-16 15:29:48 +0200

    added error type

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
-import { expandParser } from './src/parser';
-
+//import { expandParser } from './src/parser'
+import { OdatafyError, OdatafyModules } from './src/types/errors';
 export * from './src/types/nodes';
 export * from './src/parser';
 
-console.log(JSON.stringify(expandParser.parse("Address/Name"), null, 4))
+//console.log(JSON.stringify(expandParser.parse("Address/Name"), null, 4))
+try {
+throw <OdatafyError>{area: "SystemQueryOptions", originModule: OdatafyModules.Parser, queryOption:"dd"};
+} catch (e:any) {
+    console.log(JSON.stringify(e))
+}
 
 
 
