a44274450bb4fffd3da3376a280f82b391598fcb
Steffen Huels <steffen.huels@sap.com>
2022-10-02 16:28:21 +0200

    workig on some single, multi navigation tests

--- index.ts
+++ index.ts
@@ -3,13 +3,7 @@ import { expandParser } from './src/parser';
 export * from './src/types/nodes';
 export * from './src/parser';
 
-<<<<<<< HEAD
-import expandParser from './src/parsing/expandParser';
+console.log(JSON.stringify(expandParser.parse("Customer,Items"),null,4))
 
-console.log(expandParser.parse("$expand=Items,Customer($expand=Product)"))
-=======
-console.log(JSON.stringify(expandParser.parse("Name($top=4)"),null,4))
-
->>>>>>> df071af13bcfd57646bdbc3558d7ce663f66f87f
 
 
--- test/parsing/expandParser.test.ts
+++ test/parsing/expandParser.test.ts
@@ -1,99 +1,158 @@
-import assert from "assert"
-import expandParser from "../../src/parsing/expandParser"
-import { NodeTypes, ExpandNode } from "../../src/types/nodes"
+import assert from "assert";
+import expandParser from "../../src/parsing/expandParser";
+import { NodeTypes, ExpandNode } from "../../src/types/nodes";
 
-function testParsingAndAST(testcase: { type: string, input: string, expectedAST: any }) {
-    it(`should parse ${testcase.type}: ${testcase.input}`, () => {
-        let ast = expandParser.parse(testcase.input)
-        assert.deepStrictEqual(ast, testcase.expectedAST)
-    })
+function testParsingAndAST(testcase: {
+  type: string;
+  input: string;
+  expectedAST: any;
+}) {
+  it(`should parse ${testcase.type}: ${testcase.input}`, () => {
+    let ast = expandParser.parse(testcase.input);
+    assert.deepStrictEqual(ast, testcase.expectedAST);
+  });
 }
 
-describe('Expand Parser tests', () => {
-    describe('single navigation', () => {
-        [
+describe("Expand Parser tests", () => {
+  describe("single navigation", () => {
+    [
+      {
+        type: "single navigation property",
+        input: "Items",
+        expectedAST: <ExpandNode>{
+          nodeType: NodeTypes.ExpandNode,
+          value: [
             {
-                type: "single navigation property",
-                input: "Items",
-                expectedAST: <ExpandNode>{
-                    nodeType: NodeTypes.ExpandNode,
-                    value: [ {
-                        nodeType: NodeTypes.ExpandIdentifierNode,
-                        identifier: "Items"
-                    } ]
-                }
+              nodeType: NodeTypes.ExpandPathNode,
+              value: [
+                {
+                  nodeType: NodeTypes.ExpandIdentifierNode,
+                  value: "Items",
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        type: "single navigation property",
+        input: "Items/$ref",
+        expectedAST: <ExpandNode>{
+          nodeType: NodeTypes.ExpandNode,
+          value: [
+            {
+              nodeType: NodeTypes.ExpandPathNodeWithOptions,
+              optionType: "ref",
+              options: {},
+              value: [
+                {
+                  nodeType: NodeTypes.ExpandIdentifierNode,
+                  value: "Items/$ref",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ].forEach(testParsingAndAST);
+  });
+
+  describe("multi navigation", () => {
+    [
+      {
+        type: "multi navigation property",
+        input: "Customer,Items",
+        expectedAST: <ExpandNode>{
+          nodeType: NodeTypes.ExpandNode,
+          value: [
+            {
+              nodeType: NodeTypes.ExpandPathNode,
+              value: [
+                {
+                  nodeType: NodeTypes.ExpandIdentifierNode,
+                  value: "Customer",
+                },
+              ],
             },
             {
-                type: "single navigation property",
-                input: "Items/$ref",
-                expectedAST: <ExpandNode>{
-                    nodeType: NodeTypes.ExpandNode,
-                    value: [ {
-                        nodeType: NodeTypes.ExpandIdentifierNode,
-                        identifier: "Items/$ref"
-                    } ]
-                }
-            }
-
-        ].forEach(testParsingAndAST)
-    });
-
-    describe('multi navigation', () => {
-        [
+              nodeType: NodeTypes.ExpandPathNode,
+              value: [
+                {
+                  nodeType: NodeTypes.ExpandIdentifierNode,
+                  value: "Items",
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        type: "multi navigation property",
+        input: "Customer/$ref,Items",
+        expectedAST: <ExpandNode>{
+          nodeType: NodeTypes.ExpandNode,
+          value: [
             {
-                type: "multi navigation property",
-                input: "Customer,Items",
-                expectedAST: <ExpandNode>{
-                    nodeType: NodeTypes.ExpandNode,
-                    value: [ {
-                        nodeType: NodeTypes.ExpandIdentifierNode,
-                        identifier: "Customer"
-                    },
-                    {
-                        nodeType: NodeTypes.ExpandIdentifierNode,
-                        identifier: "Items"
-                    } ]
-                }
+              nodeType: NodeTypes.ExpandPathNodeWithOptions,
+              optionType: "ref",
+              options: {},
+              value: [
+                {
+                  nodeType: NodeTypes.ExpandIdentifierNode,
+                  value: "Customer",
+                },
+              ],
             },
             {
-                type: "multi navigation property",
-                input: "Customer/$ref,Items",
-                expectedAST: <ExpandNode>{
-                    nodeType: NodeTypes.ExpandNode,
-                    value: [ {
-                        nodeType: NodeTypes.ExpandIdentifierNode,
-                        identifier: "Customer/$ref"
-                    },
-                    {
-                        nodeType: NodeTypes.ExpandIdentifierNode,
-                        identifier: "Items"                    
-                    } ]
-                }
+              nodeType: NodeTypes.ExpandPathNode,
+              value: [
+                { nodeType: NodeTypes.ExpandIdentifierNode, value: "Items" },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        type: "multi navigation property, nested",
+        input: "Customer,Items($expand=Product)",
+        expectedAST: <ExpandNode>{
+          nodeType: NodeTypes.ExpandNode,
+          value: [
+            {
+              nodeType: NodeTypes.ExpandPathNode,
+              value: [
+                {
+                  nodeType: NodeTypes.ExpandIdentifierNode,
+                  value: "Customer",
+                },
+              ],
             },
             {
-                type: "multi navigation property, nested",
-                input: "Customer,Items($expand=Product)",
-                expectedAST: <ExpandNode>{
-                    nodeType: NodeTypes.ExpandNode,
-                    value: [{
-                        nodeType: NodeTypes.ExpandIdentifierNode,
-                        identifier: "Customer"
-                    },
+              nodeType: NodeTypes.ExpandPathNodeWithOptions,
+              value: [
+                { nodeType: NodeTypes.ExpandIdentifierNode, value: "Items" },
+              ],
+              options: {
+                expand: {
+                  nodeType: NodeTypes.ExpandNode,
+                  value: [
                     {
-                        nodeType: NodeTypes.ExpandIdentifierNode,
-                        identifier: "Items",
-                        options: {
-                            nodeType: NodeTypes.ExpandNode, 
-                            value: [{
-                                nodeType:NodeTypes.ExpandIdentifierNode, 
-                                identifier: "Product"
-                            }]
-                        }
-
-                    } ]
-                }
-            }
-        ].forEach(testParsingAndAST)
-    })
-})
-
+                      nodeType: NodeTypes.ExpandPathNode,
+                      value: [
+                        {
+                          nodeType: NodeTypes.ExpandIdentifierNode,
+                          value: "Product",
+                        },
+                      ],
+                    },
+                  ],
+                },
+              },
+              optionType: "default"
+            },
+          ],
+        },
+      },
+    ].forEach(testParsingAndAST);
+  });
+});
