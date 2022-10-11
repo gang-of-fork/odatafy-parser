91f6953aa076bbe798e4816c796fa28e7e4fb70e
lumaghg <rotkehlchen567@gmail.com>
2022-10-11 12:45:27 +0200

    removed index.ts

--- index.ts
+++ index.ts
@@ -1,16 +1,4 @@
-
-import { computeParser } from './src/parser';
-//import { OdatafyError, OdatafyModules } from './src/types/errors';
 export * from './src/types/nodes';
 export * from './src/parser';
-
-
-try {
-    console.log(JSON.stringify(computeParser.parse("mem"), null, 4))
-//throw <OdatafyError>{area: "SystemQueryOptions", originModule: OdatafyModules.Parser, queryOption:"dd"};
-} catch (e:any) {
-    console.log(JSON.stringify(e))
-}
-
 
 
